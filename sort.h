fefee959e5ae099b7e5102c4206e7a6a2a90927c
Levyslater <lawre@DESKTOP-9DUD6E0>
2024-01-24 00:00:43 +0300

    sort3

---
+++ sort.h
@@ -0,0 +1,33 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: intiger
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+/* prototypes */
+void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+/* Macros */
+
+#endif
